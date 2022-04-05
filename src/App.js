4727714cfaea3d03144670551ed48bc2dac816fd
John Kimball Higginson <61078573+johnkhigginson@users.noreply.github.com>
2022-04-05 13:00:10 -0600

    SPOTIFY

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login';
 import { Account } from "./components/auth/Account"
 import Status from './components/auth/Status';
 // import Settings from './components/auth/Settings';
+import SpotifyWebPlayer from 'react-spotify-web-playback/lib';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Settings /> */}
         </Account>
       </header>
+      <SpotifyWebPlayer token="BQD7WWRcU7NB3u9D0YmaCLCjLIck_32XS1lYBOyXFAt5Cypyp-yYATPoQpCp2JRN9UooG5pqVO08Lj3sd0eCyISmNb9FEIorLpWeqJ04UlJ9jRxjRtdsIxLSnAOmiHv45-noQ97ezuqSXALOc91ofqy5dwPA9gSTL6ji-A5Xorx6Y14eOl9WVgHXkuUW3HeGmW2qsD9uK3dRHN3aOA"></SpotifyWebPlayer>
     </div>
   );
 }
