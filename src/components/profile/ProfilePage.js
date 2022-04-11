3414a4a7b0c0e36377ea7d8b956ccd5ef4b18ccf
Caleb Leavitt <caleb.leavitt@domo.com>
2022-04-10 23:14:45 -0600

    Started work on StorePage

---
+++ src/components/profile/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserInfo from './UserInfo'
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <UserInfo />
+            {/* Add FavArtist component
+            Add EventsTable component */}
+        </div>
+    )
+}
---
+++ src/components/store/StorePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const StorePage = () => {
+    return (
+        //ItemsTable
+            //Item
+                //Ticket
+                //Merch
+        
+        //CartTable
+            //SelectedItem
+                //Ticket
+                //Merch
+        
+        <p>Coming soon</p>
+    )
+}
+
+export default StorePage
