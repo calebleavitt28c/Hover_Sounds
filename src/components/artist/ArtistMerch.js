9ab860856c30e1b16b8fc4af0af6c812d76ea68d
John Kimball Higginson <61078573+johnkhigginson@users.noreply.github.com>
2022-05-01 14:04:56 -0600

    adsf

--- src/components/artist/ArtistMerch.js
+++ src/components/artist/ArtistMerch.js
@@ -15,7 +15,7 @@ class ArtistMerch extends React.Component {
       <li className="border-b">
         <Link className="grid grid-cols-6" to={`/store/${this.props.artistId}`}>
         <h1 className="col-span-2">{this.props.name}</h1>
-        <h1 className="col-span-1">{this.props.price}</h1>
+        <h1 className="col-span-1">${this.props.price}</h1>
         <div className="col-span-3 flex">
           <img className="h-24 object-fit m-auto" src={this.props.image} alt={this.props.item} height="400px" width="auto"/>
         </div>
